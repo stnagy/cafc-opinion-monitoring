32c63a4c4e443a3eb3d9d418443da67a1aede4b0
Stephen Nagy <stnagy@gmail.com>
2017-11-22 15:24:57 -0500

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121133651) do
+ActiveRecord::Schema.define(version: 20171121143242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171121133651) do
     t.text "opinion_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "url"
   end
 
   create_table "search_results", force: :cascade do |t|
