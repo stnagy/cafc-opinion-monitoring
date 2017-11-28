35608f6b48c247b37640c49f1d7aed416e15fdbd
Stephen Nagy <stnagy@gmail.com>
2017-11-28 18:03:21 +0000

    hotfix?

--- clock.rb
+++ clock.rb
@@ -2,16 +2,14 @@ require 'rubygems'
 require 'clockwork'
 require 'sidekiq'
 require './config/boot'
-require './config/environment'
-# Sidekiq configuration
+require './config/environment'                                      
 
-# The following is REQUIRED to make REDIS and SIDEKIQ work on Heroku:
-# (1) Set the REDIS_PROVIDER environment variable to your redis address.
-# (2) Set the REDIS_URL environment variable to your redis address.                                        
+Sidekiq.configure_server do |config|
+  config.redis = { :url => "redis://#{ENV['DATA_REDIS_HOST']}:6379" }
+end
 
 Sidekiq.configure_client do |config|
   config.redis = { :url => "redis://#{ENV['DATA_REDIS_HOST']}:6379" }
-  config.redis = { :size => 1 }
 end
 
 # control scheduled jobs like a boss
