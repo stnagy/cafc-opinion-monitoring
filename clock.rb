4a47d0278eab652e22844a843b087852d64c90f7
Stephen Nagy <stnagy@gmail.com>
2017-11-28 14:52:46 +0000

    hotfix

--- clock.rb
+++ clock.rb
@@ -18,11 +18,11 @@ end
 
 module Clockwork
   
-  every 1.day "Update CAFC Opinions", :at => ['10:45', '10:50', '10:55', '11:00', '11:01', '11:02', '11:03', '11:04', '11:05', '11:10', '11:15'], :tz => "Eastern Time (US & Canada)" do
+  every 1.day, "Update CAFC Opinions", :at => ['10:45', '10:50', '10:55', '11:00', '11:01', '11:02', '11:03', '11:04', '11:05', '11:10', '11:15'], :tz => "Eastern Time (US & Canada)" do
   	CafcOpinionWorker.perform_async
   end 
   
-  every 1.hour "Update CAFC Opinions" do 
+  every 1.hour, "Update CAFC Opinions" do 
 		CafcOpinionWorker.perform_aync
 	end
 
