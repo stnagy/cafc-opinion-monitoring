dc08c22ee1dccdda132ee5351dab725cc398fc40
Stephen Nagy <stnagy@gmail.com>
2020-02-12 15:46:08 +0000

    set up redis info

--- config/redis.rb
+++ config/redis.rb
@@ -1,3 +1,6 @@
-$redis = Redis.new(:host => ENV['DATA_REDIS_HOST'], :port => 6379)
-redis = Redis.new(:host => ENV['DATA_REDIS_HOST'], :port => 6379)
-REDIS = Redis.new(:host => ENV['DATA_REDIS_HOST'], :port => 6379)
+# $redis = Redis.new(:host => ENV['DATA_REDIS_HOST'], :port => 6379)
+# redis = Redis.new(:host => ENV['DATA_REDIS_HOST'], :port => 6379)
+# REDIS = Redis.new(:host => ENV['DATA_REDIS_HOST'], :port => 6379)
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
