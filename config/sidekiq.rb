b855e6628f7ddf776a265b37173c636f0248d9df
Stephen Nagy <stnagy@gmail.com>
2020-02-12 15:50:01 +0000

    sidekiq config

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,7 +1,15 @@
+# Sidekiq.configure_server do |config|
+#   config.redis = { :url => "redis://#{ENV['DATA_REDIS_HOST']}:6379" }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { :url => "redis://#{ENV['DATA_REDIS_HOST']}:6379" }
+# end
+
 Sidekiq.configure_server do |config|
-  config.redis = { :url => "redis://#{ENV['DATA_REDIS_HOST']}:6379" }
+  config.redis = { :url => ENV['REDISTOGO_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "redis://#{ENV['DATA_REDIS_HOST']}:6379" }
+  config.redis = { :url => ENV['REDISTOGO_URL'] }
 end
